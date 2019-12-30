7411d0e3418271d5e83710f523a88189d2ed3605
Stephen Riley <jstephen.riley@yahoo.com>
2019-12-30 15:10:42 -0800

    expose more PCRE2 functions in our em build

--- src/lib/PCRE.js
+++ src/lib/PCRE.js
@@ -1,20 +1,21 @@
-import libpcre2 from '../../dist/libpcre2'
+import libpcre2 from '../../dist/libpcre2.js'
 import assert from 'assert'
-import {TextDecoder} from 'util'
+import { TextDecoder } from 'util'
 
 const utf16Decoder = new TextDecoder('utf-16')
 let initialized = false
 const cfunc = {}
 
 const ptrSym = Symbol('ptr')
+const nametableSym = Symbol('nametable')
 
 export default class PCRE {
-  static async init () {
+  static async init() {
     await libpcre2.loaded
 
     Object.assign(cfunc, {
-      malloc (bytes) { return libpcre2._malloc(bytes) },
-      free (ptr) { return libpcre2._free(ptr) },
+      malloc(bytes) { return libpcre2._malloc(bytes) },
+      free(ptr) { return libpcre2._free(ptr) },
       version: libpcre2.cwrap('version', 'number', ['number']),
       compile: libpcre2.cwrap('compile', 'number', ['array', 'number', 'string']),
       destroyCode: libpcre2.cwrap('destroyCode', null, ['number']),
@@ -22,12 +23,18 @@ export default class PCRE {
       lastErrorOffset: libpcre2.cwrap('lastErrorOffset', 'number'),
       match: libpcre2.cwrap('match', 'number', ['number', 'array', 'number', 'number']),
       destroyMatchData: libpcre2.cwrap('destroyMatchData', null, ['number']),
+      getOvectorCount: libpcre2.cwrap('getOvectorCount', 'number', ['number']),
+      getOvectorPtr: libpcre2.cwrap('getOvectorPointer', 'number', ['number']),
+      getCaptureCount: libpcre2.cwrap('getCaptureCount', 'number', ['number']),
+      getMatchNameCount: libpcre2.cwrap('getMatchNameCount', 'number', ['number']),
+      getMatchNameTableEntrySize: libpcre2.cwrap('getMatchNameTableEntrySize', 'number', ['number']),
+      getMatchNameTable: libpcre2.cwrap('getMatchNameTable', 'number', ['number']),
     })
 
     initialized = true
   }
 
-  static version () {
+  static version() {
     assert(initialized)
     const len = cfunc.version(0)
     const ptr = allocateStringBuffer(len)
@@ -35,7 +42,7 @@ export default class PCRE {
     return copyAndFreeStringBuffer(ptr, len)
   }
 
-  constructor (pattern, flags = '') {
+  constructor(pattern, flags = '') {
     assert(initialized)
     pattern = Buffer.from(pattern, 'utf16le')
     const ptr = cfunc.compile(pattern, pattern.length / 2, flags)
@@ -52,46 +59,163 @@ export default class PCRE {
     }
 
     this[ptrSym] = ptr
+
+    // extract the nametable 
+    const nameCount = this.getMatchNameCount();
+    const entrySize = this.getMatchNameTableEntrySize();
+    const tableBuf = this.getMatchNameTable();
+    this[nametableSym] = convertNameTable(tableBuf, nameCount, entrySize)
   }
 
-  destroy () {
+  destroy() {
     if (this[ptrSym] === 0) return
     cfunc.destroyCode(this[ptrSym])
     this[ptrSym] = 0
   }
 
-  exec (subject, options) {
+  destroyMatchData(matchDataPtr) {
+    cfunc.destroyMatchData(matchDataPtr)
+  }
+
+  exec(subject, options) {
+    return this.match(subject, options)
+  }
+
+  match(subject, options) {
     assert(this[ptrSym])
 
-    const {startOffset} = {
+    if (typeof options === 'string') {
+      options = {
+        startOffset: 0,
+        i: options.includes('i'),
+        s: options.includes('s'),
+        m: options.includes('m'),
+        g: options.includes('g'),
+      }
+    }
+
+    const { startOffset } = {
       startOffset: 0,
       ...options
     }
 
-    subject = Buffer.from(subject, 'utf16le')
-    const matchDataPtr = cfunc.match(
+    const subjectBuffer = Buffer.from(subject, 'utf16le')
+    const matchData = cfunc.match(
       this[ptrSym],
-      subject, subject.length / 2,
+      subjectBuffer,
+      subjectBuffer.length / 2,
       startOffset
     )
 
-    cfunc.destroyMatchData(matchDataPtr)
-    return null
+    // extract the matches from the pcre2_match_data block
+    const matchCount = this.getOvectorCount(matchData);
+    if (matchCount === 0) {
+      this.destroyMatchData(matchData)
+      return null
+    }
+    const vectorPtr = this.getOvectorPtr(matchData);
+    const matches = convertOVector(subject, vectorPtr, matchCount)
+
+    if (matches[0].start === 0 && matches[0].end === 0) {
+      this.destroyMatchData(matchData)
+      return null
+    }
+
+    // merge in nametable entries
+    const results = { ...matches }
+    for (let i in matches) {
+      if (i in this[nametableSym]) {
+        const name = this[nametableSym][i]
+        results[name] = matches[i]
+      }
+    }
+
+    this.destroyMatchData(matchData)
+
+    results.length = matchCount
+    return results
+  }
+
+  getOvectorCount(matchDataPtr) {
+    assert(matchDataPtr)
+    return cfunc.getOvectorCount(matchDataPtr)
+  }
+
+  getOvectorPtr(matchDataPtr) {
+    assert(matchDataPtr)
+    return cfunc.getOvectorPtr(matchDataPtr)
+  }
+
+  getCaptureCount() {
+    if (this[ptrSym] === 0) return
+    return cfunc.getCaptureCount(this[ptrSym])
+  }
+
+  getMatchNameCount() {
+    if (this[ptrSym] === 0) return
+    return cfunc.getMatchNameCount(this[ptrSym])
+  }
+
+  getMatchNameTableEntrySize() {
+    if (this[ptrSym] === 0) return
+    return cfunc.getMatchNameTableEntrySize(this[ptrSym])
+  }
+
+  getMatchNameTable() {
+    if (this[ptrSym] === 0) return
+    return cfunc.getMatchNameTable(this[ptrSym])
   }
 }
 
-function allocateStringBuffer (len) {
+function allocateStringBuffer(len) {
   return cfunc.malloc(len * 2)
 }
 
-function copyStringBuffer (ptr, len) {
+function copyStringBuffer(ptr, len) {
   len = libpcre2.HEAPU16[(ptr / 2) + (len - 1)] === 0 ? len - 1 : len
   const encodedString = libpcre2.HEAP8.subarray(ptr, ptr + (len * 2))
   return utf16Decoder.decode(encodedString)
 }
 
-function copyAndFreeStringBuffer (ptr, len) {
+function copyAndFreeStringBuffer(ptr, len) {
   const string = copyStringBuffer(ptr, len)
   cfunc.free(ptr)
   return string
+}
+
+function convertOVector(subject, vectorPtr, vectorCount) {
+  const table = []
+
+  for (let i = 0; i < vectorCount; i++) {
+    let ptr = vectorPtr + i * 4 * 2
+    const start = libpcre2.getValue(ptr, 'i32', false)
+    const end = libpcre2.getValue(ptr + 4, 'i32', false)
+    const match = subject.substring(start, end)
+    table.push({ start, end, match })
+  }
+
+  return table
+}
+
+function convertNameTable(nameTablePtr, entries, entrySize) {
+  const table = {}
+
+  for (let i = 0; i < entries; i++) {
+    let ptr = nameTablePtr + entrySize * i * 2
+
+    const index = libpcre2.getValue(ptr, 'i16', false)
+    const name = copyStringBuffer(ptr + 2, utf16lelen(ptr + 2))
+    table[index] = name
+  }
+
+  return table
+}
+
+function utf16lelen(ptr) {
+  let len = 0
+  while (libpcre2.getValue(ptr, 'i16', false) !== 0) {
+    len++
+    ptr += 2
+  }
+  return len
 }
--- src/lib/libpcre2.c
+++ src/lib/libpcre2.c
@@ -9,88 +9,152 @@ static int _lastErrorCode = 0;
 static PCRE2_SIZE _lastErrorOffset = 0;
 
 EMSCRIPTEN_KEEPALIVE
-PCRE2_SIZE lastErrorMessage (uint16_t* buffer, size_t bufferLength) {
+PCRE2_SIZE lastErrorMessage(uint16_t *buffer, size_t bufferLength)
+{
   // Going to trust that buffer is large enough for all messages, because
   // we are the only callers of this function, and we also know that
   // PCRE2 error messages are max 120 characters long.
-  if (_lastErrorCode == 0) return 0;
+  if (_lastErrorCode == 0)
+    return 0;
   return pcre2_get_error_message(_lastErrorCode, buffer, bufferLength);
 }
 
 EMSCRIPTEN_KEEPALIVE
-PCRE2_SIZE lastErrorOffset () {
+PCRE2_SIZE lastErrorOffset()
+{
   return _lastErrorOffset;
 }
 
 // --------------------------------------------------------------------
 
 EMSCRIPTEN_KEEPALIVE
-pcre2_code* compile (
-  uint16_t* pattern,
-  PCRE2_SIZE length,
-  char* flags
-) {
+pcre2_code *compile(
+    uint16_t *pattern,
+    PCRE2_SIZE length,
+    char *flags)
+{
   // Clear any previous error codes.
   _lastErrorOffset = 0;
   _lastErrorCode = 0;
 
   // Convert flags to PCRE2 options bitfield
   int options = PCRE2_UTF;
-  for (int i = 0; flags[i] != 0; i++) {
-    switch (flags[i]) {
-      case 'm': options |= PCRE2_MULTILINE; break;
-      case 's': options |= PCRE2_DOTALL; break;
-      case 'i': options |= PCRE2_CASELESS; break;
-      case 'x': options |= PCRE2_EXTENDED; break;
-      default:
-        _lastErrorCode = PCRE2_ERROR_BAD_OPTIONS;
-        return NULL;
+  for (int i = 0; flags[i] != 0; i++)
+  {
+    switch (flags[i])
+    {
+    case 'm':
+      options |= PCRE2_MULTILINE;
+      break;
+    case 's':
+      options |= PCRE2_DOTALL;
+      break;
+    case 'i':
+      options |= PCRE2_CASELESS;
+      break;
+    case 'x':
+      options |= PCRE2_EXTENDED;
+      break;
+    default:
+      _lastErrorCode = PCRE2_ERROR_BAD_OPTIONS;
+      return NULL;
     }
   }
 
   return pcre2_compile(
-    pattern, length, options,
-    &_lastErrorCode, &_lastErrorOffset,
-    NULL
-  );
+      pattern, length, options,
+      &_lastErrorCode, &_lastErrorOffset,
+      NULL);
 }
 
 EMSCRIPTEN_KEEPALIVE
-void destroyCode (pcre2_code* code) {
+void destroyCode(pcre2_code *code)
+{
   return pcre2_code_free(code);
 }
 
 // --------------------------------------------------------------------
 
 EMSCRIPTEN_KEEPALIVE
-pcre2_match_data* match (
-  pcre2_code* code,
-  uint16_t* subject,
-  PCRE2_SIZE length,
-  PCRE2_SIZE offset
-) {
-  pcre2_match_data* result = pcre2_match_data_create_from_pattern(code, NULL);
+pcre2_match_data *match(
+    pcre2_code *code,
+    uint16_t *subject,
+    PCRE2_SIZE length,
+    PCRE2_SIZE offset)
+{
+  pcre2_match_data *result = pcre2_match_data_create_from_pattern(code, NULL);
 
-  // Commented out pcre2_match call because including it causes
-  // WebAssembly compiler in V8 to go into a long running and memory
-  // hungry loop. It seems to be fixed in V8 6.9, however Node.js
-  // currently runs V8 6.7. Unfortunately V8 6.8 is broken as well, and
-  // so it is not clear if this will get fixed until Node.js 11.0, which
-  // will use V8 7.0.
-
-  // pcre2_match(code, subject, length, offset, 0, result, NULL);
+  pcre2_match(code, subject, length, offset, 0, result, NULL);
 
   return result;
 }
 
 EMSCRIPTEN_KEEPALIVE
-void destroyMatchData (pcre2_match_data* data) {
+void destroyMatchData(pcre2_match_data *data)
+{
   return pcre2_match_data_free(data);
 }
 
 // --------------------------------------------------------------------
 
 EMSCRIPTEN_KEEPALIVE
-size_t version (uint16_t* result) {
+size_t version(uint16_t *result)
+{
   return pcre2_config(PCRE2_CONFIG_VERSION, result);
+}
+
+// --------------------------------------------------------------------
+
+EMSCRIPTEN_KEEPALIVE
+size_t getOvectorCount(pcre2_match_data *match_data)
+{
+  return pcre2_get_ovector_count(match_data);
+}
+
+// --------------------------------------------------------------------
+
+EMSCRIPTEN_KEEPALIVE
+PCRE2_SIZE *getOvectorPointer(pcre2_match_data *match_data)
+{
+  return pcre2_get_ovector_pointer(match_data);
+}
+
+// --------------------------------------------------------------------
+
+EMSCRIPTEN_KEEPALIVE
+PCRE2_SIZE getCaptureCount(pcre2_code *code)
+{
+  size_t count;
+  pcre2_pattern_info(code, PCRE2_INFO_CAPTURECOUNT, &count);
+  return count;
+}
+
+// --------------------------------------------------------------------
+
+EMSCRIPTEN_KEEPALIVE
+PCRE2_SIZE getMatchNameCount(pcre2_code *code)
+{
+  size_t count;
+  pcre2_pattern_info(code, PCRE2_INFO_NAMECOUNT, &count);
+  return count;
+}
+
+// --------------------------------------------------------------------
+
+EMSCRIPTEN_KEEPALIVE
+PCRE2_SIZE getMatchNameTableEntrySize(pcre2_code *code)
+{
+  size_t entry_size;
+  pcre2_pattern_info(code, PCRE2_INFO_NAMEENTRYSIZE, &entry_size);
+  return entry_size;
+}
+
+// --------------------------------------------------------------------
+
+EMSCRIPTEN_KEEPALIVE
+PCRE2_SIZE getMatchNameTable(pcre2_code *code)
+{
+  size_t table_size;
+  pcre2_pattern_info(code, PCRE2_INFO_NAMETABLE, &table_size);
+  return table_size;
 }
